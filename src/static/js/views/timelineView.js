cfa5a0ce267082b77b63afba83fb0d87d97acc53
dsandovalm <58996746+dsandovalm@users.noreply.github.com>
2021-05-12 13:29:54 -0500

    Pop Up

--- src/static/js/views/timelineView.js
+++ src/static/js/views/timelineView.js
@@ -1,3 +1,4 @@
+import { newReport, newReportAfterRender } from './postPopUp.js'
 export const renderTimeline = () => {
   const html = `
       <section id="muro">
@@ -14,9 +15,12 @@ export const renderTimeline = () => {
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d254508.3928106861!2d-74.24789140391782!3d4.648625932164217!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9bfd2da6cb29%3A0x239d635520a33914!2zQm9nb3TDoQ!5e0!3m2!1ses!2sco!4v1618855424331!5m2!1ses!2sco"
           style="border:0;" allowfullscreen="" loading="lazy"></iframe>
       </div>
+      
+      <div id="modal"></div>
 
       <footer>
-        <img class="icon-profile" src="static/images/icons/icon-profile.png">
+        <img id="icon-profile" class="icon-profile" src="static/images/icons/icon-profile.png">
+        <img id="icon-report" class="icon-report" src="static/images/icons/icon-settings.png">
         <img id="icon-settings" class="icon-settings" src="static/images/icons/icon-settings.png">
       </footer>
 
@@ -27,3 +31,33 @@ export const renderTimeline = () => {
 
   return div;
 };
+
+function openPopUp(div) {
+  console.log(div);
+  document.getElementById('modal').style.display = 'block';
+  document.getElementById('modal').innerHTML = '';
+  document.getElementById('modal').appendChild(div);
+}
+
+function closePopUp() {
+  document.getElementById('modal').style.display = 'none';
+}
+
+export function afterRenderTimeline() {
+
+  const iconProfile = document.querySelector('#icon-profile');
+  iconProfile.addEventListener('click', () => {
+    window.location.assign('#/profile');
+  });
+
+  const iconReport = document.querySelector('#icon-report');
+  iconReport.addEventListener('click', () => {
+    openPopUp(newReport());
+    newReportAfterRender(closePopUp);
+  });
+
+  const iconSettings = document.querySelector('#icon-settings');
+  iconSettings.addEventListener('click', () => {
+    window.location.assign('#/settings');
+  });
+}
