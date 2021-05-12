101a20284259aac96dc9c5dccfa0405d8f6e98c5
dsandovalm <58996746+dsandovalm@users.noreply.github.com>
2021-05-12 08:00:40 -0500

    Interface de posts

--- src/static/js/model/post.js
+++ src/static/js/model/post.js
@@ -1,23 +1,25 @@
-function createPost( ubicationReport, photoReport, descriptionReport ) {
-  let post = {
-    date: new Date(),
-    ubication: ubicationReport,
-    photo: photoReport,
-    description: descriptionReport,
-    stats: {
-      like: 0,
-      meh: 0,
-      dislike: 0,
-      views: 0
+export function createPost( ubicationReport, photoReport, descriptionReport ) {
+    let post = {
+        date: new Date(),
+        ubication: ubicationReport,
+        photo: photoReport,
+        description: descriptionReport,
+        stats: {
+            like: 0,
+            meh: 0,
+            dislike: 0,
+            views: 0
+        }
     }
-  }
-  
-  db.collection("Posts").add({ post })
-  .then((docRef) => {
-    console.log("Document written with ID: ", docRef.id);
-  })
-  .catch((error) => {
-    console.error("Error adding document: ", error);
-  }); 
-  return post
+        
+    firebase.
+    firestore().
+    collection("Posts").add({ post })
+    .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+    }); 
+    return post
 }
