0aba49bf50e33ceab2c9447b83ba1043af4c3d5d
dsandovalm <58996746+dsandovalm@users.noreply.github.com>
2021-05-13 13:16:50 -0500

    After render

--- src/static/js/views/postPopUp.js
+++ src/static/js/views/postPopUp.js
@@ -1,10 +1,10 @@
-import { createPost } from "../model/post.js";
+import { createPost, reactPost } from "../model/post.js";
 
 export const newReport = () => {
     let htmlText = `
     <h2>Nuevo Reporte</h2>
     <div>
-        <div> 
+        <div>
             <label for="reportType">Tipo de reporte</label> 
             <select name="reportType" id="reportType">
             <option value="accidente">Accidente</option>
@@ -45,7 +45,7 @@ export function newReportAfterRender(closeFunction) {
         let description = document.getElementById('reportDesc').value;
         let repImage = document.getElementById('uploadImg').files[0];
         let repType = document.getElementById('reportType').value;
-        createPost('0,0', repType, repImage, description);
+        createPost({ lat: 0, lng: 0 }, repType, repImage, description);
     });
 
     const btnClose = document.getElementById('close');
@@ -55,47 +55,63 @@ export function newReportAfterRender(closeFunction) {
 
 export const showReport = (user,post) => {
     let htmlText = `
-<div>
-  <div> 
-    <img class="profile-small" src="${user.profilepic}">
     <div>
-      <div>
-        <span class="star"></span>
-        <span class="star"></span>
-        <span class="star"></span>
-        <span class="star"></span>
-        <span class="star"></span>
-      </div>
-      <p>${user.name}</p>
-  </div>
-  <div>
-    <p>${post.date}</p>
-    <p>Tipo de Reporte</p>
-   <p>${post.type}</p>
-  </div>
-</div>
-
-<div>
-  <div>
-    <h3>Descripción del reporte</h3>
-    <p>${post.description}</p>
-  </div>
-  <img src="${post.img}"> 
-</div>
-
-<div class="reactions">
-  <div class="btn-reaction like"> 
-    <img src="" alt="Es Útil">
-  </div>
-  <div class="btn-reaction doubt"> 
-    <img src="" alt="Lo dudo">
-  </div>
-  <div class="btn-reaction dislike"> 
-    <img src="" alt="No es Útil">
-  </div>
-</div>`;
+        <div> 
+            <img class="profile-small" src="${user.profilepic}">
+            <div>
+                <div>
+                    <span class="star"></span>
+                    <span class="star"></span>
+                    <span class="star"></span>
+                    <span class="star"></span>
+                    <span class="star"></span>
+                </div>
+                <p>${user.name}</p>
+            </div>
+        </div>
+        <div>
+            <p>${post.date}</p>
+            <p>Tipo de Reporte</p>
+            <p>${post.type}</p>
+        </div>
+    </div>
+    <div>
+        <div>
+            <h3>Descripción del reporte</h3>
+            <p>${post.description}</p>
+        </div>
+        <img src="${post.img}"> 
+    </div>
+    <div class="reactions">
+        <div class="btn-reaction-like" id="btnLike"> 
+            <img src="" alt="Es Útil">
+        </div>
+        <div class="btn-reaction-doubt" id="btnDoubt"> 
+            <img src="" alt="Lo dudo">
+        </div>
+        <div class="btn-reaction-dislike" id="btnDislike"> 
+            <img src="" alt="No es Útil">
+        </div>
+    </div>`;
     const div = document.createElement('div');
     div.className = 'report';
     div.innerHTML = htmlText;
     return div;
+}
+
+export function newReportAfterRender(postId /*, closeFunction*/ ){
+    const btnLike = document.getElementById('btnLike');
+    btnLike.addEventListener('click', () => {
+        reactPost (postId,'like')
+    });
+    const btnDoubt = document.getElementById('btnDoubt');
+    btnDoubt.addEventListener('click', () => {
+        reactPost (postId,'doubt')
+    });
+    const btnDislike = document.getElementById('btnDislike');
+    btnDislike.addEventListener('click', () => {
+        reactPost (postId,'dislike')
+    });
+    // const btnClose = document.getElementById('close');
+    // btnClose.addEventListener('click', closeFunction);
 }
