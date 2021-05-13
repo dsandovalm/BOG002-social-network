1f258c240e87c84c6b8ee869982cd574d1c650e9
dsandovalm <58996746+dsandovalm@users.noreply.github.com>
2021-05-13 14:57:41 -0500

    Cambio de nombre para newReport

--- src/static/js/views/timelineView.js
+++ src/static/js/views/timelineView.js
@@ -1,4 +1,4 @@
-import { newReport, newReportAfterRender } from './postPopUp.js'
+import { modalCreate } from './postPopUp.js'
 export const renderTimeline = () => {
   const html = `
       <section id="muro">
@@ -10,13 +10,15 @@ export const renderTimeline = () => {
         <img id=icon-lupa src="static/images/icons/icon-lupa.png">
       </header>
 
-      <div id="map">
+      <div id="map1">
         <iframe id="mapa"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d254508.3928106861!2d-74.24789140391782!3d4.648625932164217!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9bfd2da6cb29%3A0x239d635520a33914!2zQm9nb3TDoQ!5e0!3m2!1ses!2sco!4v1618855424331!5m2!1ses!2sco"
           style="border:0;" allowfullscreen="" loading="lazy"></iframe>
       </div>
       
-      <div id="modal"></div>
+      <div id="modal">
+      :)
+      </div>
 
       <footer>
         <img id="icon-profile" class="icon-profile" src="static/images/icons/icon-profile.png">
@@ -52,12 +54,14 @@ export function afterRenderTimeline() {
 
   const iconReport = document.querySelector('#icon-report');
   iconReport.addEventListener('click', () => {
-    openPopUp(newReport());
-    newReportAfterRender(closePopUp);
+    openPopUp(modalCreate.render());
+    modalCreate.afterRender(closePopUp);
   });
 
   const iconSettings = document.querySelector('#icon-settings');
   iconSettings.addEventListener('click', () => {
+    console.log('clickonsettings');
     window.location.assign('#/settings');
   });
+
 }
