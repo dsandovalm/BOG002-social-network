4f30da270fa0e3eab6aea81ab55907debb0b8b23
dsandovalm <58996746+dsandovalm@users.noreply.github.com>
2021-05-14 13:59:04 -0500

    Calculos de la posicion y css como comentarios

--- src/static/js/views/postBubbles.js
+++ src/static/js/views/postBubbles.js
@@ -7,7 +7,25 @@ export const bubble = {
         let div = document.createElement('div');
         div.className = "bubble";
         div.setAttribute('id', post.id);
-        div.appendChild(img)
+        div.appendChild(img);
+        /*  ANOTACIONES
+        let map = {
+            latmin: minima latitud en el mapa,
+            latmax: maxima latitud en el mapa,
+            lngmin: minima longitud en el mapa,
+            lngmax: maxima longitud en el mapa,
+            w: ancho del mapa,
+            h: alto del mapa,
+            // No sabria de donde sacarlas
+        };
+        
+        // - - - CSS
+
+        div.style.position = 'absolute';
+        div.style.top = (map.h * (post.lng - map.lngmin) / (map.lngmax-map.lngmin) );
+        div.style.left = (map.w * (post.lat - map.latmin) / (map.latmax-map.latmin) );
+        // Podria pasarsele el mapa como parametro y que se agreguen alli mismo
+        */
         return div;
     },
     afterRender: (post, openFunction) => {
