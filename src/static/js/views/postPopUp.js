0376de96bed17e21bd2fce1d05140e667e18a332
dsandovalm <58996746+dsandovalm@users.noreply.github.com>
2021-05-18 08:53:44 -0500

    Update postPopUp.js

--- src/static/js/views/postPopUp.js
+++ src/static/js/views/postPopUp.js
@@ -3,33 +3,44 @@ import { createPost, reactPost } from "../model/post.js";
 // He decidido meter acá todo lo relacionado con la visualización de posts
 
 const modal = {
-    create: `<h2>Nuevo Reporte</h2>
-        <div>
+    create: `<div class="flex">
+        <div class="flex mid ">
+          <img class="profile-small" src="${user.profilepic}">
+          <div>
             <div>
-                <label for="reportType">Tipo de reporte</label> 
-                <select name="reportType" id="reportType">
-                <option value="accidente">Accidente</option>
-                <option value="bloqueo">Bloqueo</option>
-                <option value="infraccion">Infraccion</option>
-                <option value="cierre">Cierre</option>
-                <option value="manifestacion">Manifestacion</option>
-                <option value="trafico">Tráfico</option>
-                </select>
+              <span class="star"></span>
+              <span class="star"></span>
+              <span class="star"></span>
+              <span class="star"></span>
+              <span class="star"></span>
             </div>
-            <div> Fecha
-                <label for=""></label>
-                <input name="" id="" type="date">
-            </div>
-            <div>
-                <label for="uploadImg">Foto evidencia</label>
-                <input name="uploadImg" id="uploadImg" type="file">
-            </div>
-            <div>
-                <label for="reportDesc">Descripcion</label>
-                <input name="reportDesc" id="reportDesc" type="text">
-            </div>
+            <p>${user.name}</p>
+          </div>
         </div>
-        <button class="btn" id="btnPost">Publicar</button>`,
+        <div class="mid">
+          <p>${post.date}</p>
+          <p>Tipo de Reporte</p>
+          <p>${post.type}</p>
+        </div>
+      </div>
+      <div class="flex">
+        <div class="mid">
+          <h3>Descripción del reporte</h3>
+          <p>${post.description}</p>
+        </div>
+        <img src="${post.img}">
+      </div>
+      <div class="reactions">
+        <div class="btn-reaction-like" id="btnLike">
+          <img src="" alt="Es Útil">
+        </div>
+        <div class="btn-reaction-doubt" id="btnDoubt">
+          <img src="" alt="Lo dudo">
+        </div>
+        <div class="btn-reaction-dislike" id="btnDislike">
+          <img src="" alt="No es Útil">
+        </div>
+      </div>`,
     view(post,user){
         return `<div>
             <div> 
