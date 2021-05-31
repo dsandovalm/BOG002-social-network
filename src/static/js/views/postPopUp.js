c3e8ee321a5b4e4a13df1659ef62ace335ac00fd
dsandovalm <58996746+dsandovalm@users.noreply.github.com>
2021-05-31 07:27:42 -0500

    Update postPopUp.js

--- src/static/js/views/postPopUp.js
+++ src/static/js/views/postPopUp.js
@@ -1,142 +1,146 @@
-import { createPost, reactPost } from '../model/post.js';
-//import { geoFindMe } from '../model/location.js'
+import { db } from '../controler/firebase_config.js';
 
-// He decidido meter acá todo lo relacionado con la visualización de posts
+export function createPost(typeReport, photoReport, descriptionReport ) {
+    
+    function success(pos) {
 
-const modal = {
-    create: `<h2>Nuevo Reporte</h2>
-        <div>
-            <div>
-                <label for="reportType">Tipo de reporte</label> 
-                <select name="reportType" id="reportType">
-                <option value="accidente">Accidente</option>
-                <option value="bloqueo">Bloqueo</option>
-                <option value="infraccion">Infraccion</option>
-                <option value="cierre">Cierre</option>
-                <option value="manifestacion">Manifestacion</option>
-                <option value="trafico">Tráfico</option>
-                </select>
-            </div>
-            <div>
-                <label for="uploadImg">Foto evidencia</label>
-                <input name="uploadImg" id="uploadImg" type="file" required>
-            </div>
-            <div>
-                <label for="reportDesc">Descripcion</label>
-                <input name="reportDesc" id="reportDesc" type="text" autocomplete="off" required>
-            </div>
-        </div>
-        <button class="btn" id="btnPost">Publicar</button>`,
-    view(post,user,time,url){
-        return `<div class="flex">
-            <div class="mid ">
-                <div class="flex">
-                    <img class="profile-small" src="${user.profilepic}">
-                    <div>
-                        <div>
-                            <span class="star"></span>
-                            <span class="star"></span>
-                            <span class="star"></span>
-                            <span class="star"></span>
-                            <span class="star"></span>
-                        </div>
-                        <p>${user.name}</p>
-                    </div>
-                </div>
-                <div>
-                    <p>${time}</p>
-                    <p>Tipo de Reporte</p>
-                    <p>${post.type}</p>
-                </div>
-                <div>
-                    <h3>Descripción del reporte</h3>
-                    <p>${post.description}</p>
-                </div>
-            </div>
-            <div class="mid">
-                <h3>Foto Evidencia</h3>
-                <img class="mid" src="${url}">
-                <div class="reactions">
-                    <div class="btn-reaction-like" id="btnLike">
-                        <img src="static/images/icons/btn-like.png" alt="Es Útil">
-                    </div>
-                    <div class="btn-reaction-doubt" id="btnDoubt">
-                        <img src="static/images/icons/btn-doubt.png" alt="Lo dudo">
-                    </div>
-                    <div class="btn-reaction-dislike" id="btnDislike">
-                        <img src="static/images/icons/btn-dislike.png" alt="No es Útil">
-                    </div>
-                </div>
-            </div>
-        </div>`
-    },
-    render(htmlText) {
-        const div = document.createElement('div');
-        div.className = 'report'; // En serio es necesaria la clase?
-        div.innerHTML = `<img id = "close" src = "static/images/icons/icon-close.png">${htmlText}`;
-        return div;
-    },
-    afterRender(functions){
-        functions.forEach(f => {
-            let btn = document.getElementById(f.id);
-            btn.addEventListener('click', f.method);
+        // Obtiene coordenadas (latitud y longitud)
+        let crd = pos.coords;
+        //  Crea un objeto post
+        const post = {
+            ubication: [ crd.latitude, crd.longitude ],
+            type: typeReport,
+            description: descriptionReport,
+            user: firebase.auth().currentUser.uid,
+            date: new Date(),
+            stats: {
+                like:[],
+                doubt:[],
+                dislike:[],
+            }
+        }
+        
+        // Guarda el objeto post en el firestore
+        firebase.firestore().
+        collection("Posts").add( post )
+        .then((docRef) => {
+            console.log("Document written with ID: ", docRef.id);
+            
+            // Guarda la foto en storage
+            const photoRef = firebase.storage().ref().child(`images/${docRef.id}.jpg`);
+            photoRef.put(photoReport).then((snapshot) => {
+                console.log('Se ha subido una foto!', snapshot);
+                return snapshot;
+            });
+
+            // Añade la direccion de la foto al campo photo del post
+            firebase.firestore().
+            collection("Posts").doc(docRef.id).set({
+                photo: `images/${docRef.id}.jpg`
+            }, { merge: true }).then(() => {
+                console.log("Se ha guardado la dirección de la foto");
+            }).catch((error) => {
+                console.error("Error al subir la foto: ", error);
+            });
+        }).catch((error) => {
+        console.error('Error al crear el post: ', error);
         });
-    },
+
+        };
+      
+      function error(err) {
+        console.warn('ERROR(' + err.code + '): ' + err.message);
+      };
+      
+    return navigator.geolocation.getCurrentPosition(success, error);
+    
 }
 
-const buttons = {
-    close: (closeFunction) => {
-        return {
-            id:'close', 
-            method: closeFunction
-        }
-    },
-    create: {
-        id:'btnPost', 
-        method: () => {
-            let description = document.getElementById('reportDesc').value;
-            let repImage = document.getElementById('uploadImg').files[0];
-            let repType = document.getElementById('reportType').value;
-            createPost( repType, repImage, description);}
-    }, 
-    like: (postId) => {
-        return {
-            id:'btnLike', 
-            method: () => { reactPost (postId,'like') }
-        }
-    },
-    doubt: (postId) => {
-        return {
-            id:'btnDoubt', 
-            method: () => { reactPost (postId,'doubt') } 
-        }
-    },
-    dislike: (postId) => {
-        return {
-            id:'btnDislike', 
-            method: () => { reactPost (postId,'dislike') }
-        }
-    },
+export const deletePost = (postId) =>{
+    db.collection('Posts').doc(postId).delete().then(() => {
+        console.log("Document successfully deleted!");
+    }).catch((error) => {
+        console.error("Error removing document: ", error);
+    });
 }
 
-export const modalCreate = {
-    render() { return modal.render(modal.create); },
-    afterRender(closeFunction){
-        modal.afterRender([buttons.close(closeFunction), buttons.create]);
+export function calculatePoints(post){
+    let likes = post.stats.like.length;
+    let dislikes = post.stats.dislike.length;
+    let points = likes - dislikes;
+    return points;  
+}
+
+function isLiked(post){
+    let result = {
+        isThis: false
     }
-}
-
-export const modalView = {
-    render(post,user,time,url) {
-        return modal.render(modal.view(post,user,time,url));
-    },
-    afterRender(closeFunction,postId){
-        modal.afterRender([
-            buttons.close(closeFunction),
-            buttons.like(postId) , 
-            buttons.doubt(postId),
-            buttons.dislike(postId)
-        ]);
+    for (const react in post.stats) {
+        console.log(post.stats[react])
+        let index = post.stats[react].indexOf( firebase.auth().currentUser.uid );
+        if ( index != -1 ) {
+            result = {
+                isThis: true,
+                reaction: react,
+                index: index,
+            }
+        }
     }
+    return result
 }
 
+
+
+export function reactPost (postId,likeType){
+    
+    // Toma el post y le añade 1 like
+    let postRef = firebase.firestore().collection("Posts").doc(postId);
+    postRef.get().then( (post) => {
+        
+        /*
+        let current = {}
+
+        for (const reaction in post.data().stats) {
+            current[reaction] = post.data().stats[reaction]
+            let index = current[reaction].indexOf( firebase.auth().currentUser.uid );
+            if( index != -1 ) {
+                current[reaction].splice( index, 1 );
+            }
+        }
+
+        switch(likeType){
+            case 'like':
+                current.like.push( firebase.auth().currentUser.uid );
+                break;
+            case 'dislike':
+                current.dislike.push( firebase.auth().currentUser.uid );
+                break;
+            case 'doubt':
+                current.doubt.push( firebase.auth().currentUser.uid );
+                break;
+            default:
+                break;
+        }
+        */
+        
+        let current = post.data().stats;
+        // Revisa que el usuario no tenga likes en el post
+        let liked = isLiked( post.data() );
+
+        // 3 casos: liked.isThis = false, liked.reaction === likeType, liked.reaction != likeType
+        if( liked.isThis ) {
+            current[ liked.reaction ].splice( liked.index, 1 );
+            if( liked.reaction != likeType ) {
+                current[likeType].push( firebase.auth().currentUser.uid );
+            }
+        } else {
+            current[likeType].push( firebase.auth().currentUser.uid );
+        }
+
+        postRef.update({
+            stats: current
+        })
+    }).catch((error) => {
+        console.error("Error writing document: ", error);
+    });    
+}
