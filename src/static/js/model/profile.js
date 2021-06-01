fc490c135ce07b6b27282738c8e0cc50978960c9
nasarmientoo <nasarmientoo@correo.udistrital.edu.co>
2021-06-01 08:29:02 -0500

    Ultimas actualizaciones

--- src/static/js/model/profile.js
+++ src/static/js/model/profile.js
@@ -8,16 +8,20 @@ export function getUser() {
 }
 
 export function getDataPost(container) {
-  let post = db.collection('Posts').where('user', '==', firebase.auth().currentUser.uid)
+  var post = db.collection('Posts').where('user', '==', firebase.auth().currentUser.uid)
     .get().then((snapshot) => {
-      snapshot.forEach((doc) => { 
-        let photoRef = firebase.storage().ref().child(doc.data().photo);
+      snapshot.forEach((doc) => {
+        var photoRef = firebase.storage().ref().child(doc.data().photo);
         photoRef.getDownloadURL().then(function (url) {
-          let html = `
+          var btnId = `${doc.id}btn`
+          var listId = `${doc.id}list`
+          var html = `
                   <div id = "data-report">
                     <div id = "dropdown-post">
-                      <button class = "dropdown-btn"><img  src = "static/images/icons/icon-options.png"></button>
-                      <ul class = "options-dropdown">
+                      <div class = "jsBtnContainer">
+                      <button id = "${btnId}" class = "optionButtons"><img  src = "static/images/icons/icon-options.png"></button>
+                      </div>
+                      <ul id = "${listId}" class = "options-dropdown">
                         <li><button>Editar Post</button></li>
                         <li><button>Eliminar Post</button></li>
                       </ul>
@@ -27,7 +31,8 @@ export function getDataPost(container) {
                     <img id="img-report" class ="img-report" src = "${url}"><br>
                     <img id="point-like" src = "static/images/icons/btn-like.png"><img id="point-question" src = "static/images/icons/btn-doubt.png"><img id="point-dislike" src = "static/images/icons/btn-dislike.png">
                   </div>`
-          container.innerHTML += html;      
+          container.innerHTML += html;
+
         })
       }).catch((error) => {
         console.log('error 1')
